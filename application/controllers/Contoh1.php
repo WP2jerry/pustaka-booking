6d9294ed2b6b5156ba56762df09b0c018f47aaab
WP2jerry <pneumajes@gmail.com>
2022-03-22 21:43:45 +0700

    pertama commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+class Contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Jerry Indrayanto Baka
+        saya tinggal di Daerah Jakarta Pusat
+        hobby saya Music dan olah raga 
+        Olah Raga Sepak bola, Basket, Futsal serta Pencak Silat dan Aikido.
+        Alat musik yang saya kuasai adalah Drum";
+    }
+}
+?>
